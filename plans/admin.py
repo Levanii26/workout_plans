f4c6366146896e363e6faf60456648ec9a66f762
Levanchutlashvili <131532268+Levanii26@users.noreply.github.com>
2024-03-10 00:24:08 +0400

    register all models

--- plans/admin.py
+++ plans/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Exercise, WorkoutPlan, ExercisePlan, ExerciseInstance, UserWeight, UserFitnessGoal
 
-# Register your models here.
+admin.site.register(Exercise)
+admin.site.register(WorkoutPlan)
+admin.site.register(ExercisePlan)
+admin.site.register(ExerciseInstance)
+admin.site.register(UserWeight)
+admin.site.register(UserFitnessGoal)
--- workout_plans/urls.py
+++ workout_plans/urls.py
@@ -6,7 +6,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Workout Plans API",
+      title="fitness-api.p.rapidapi.com",
       default_version='v1',
       description="API for managing personalized workout plans",
       terms_of_service="https://www.example.com/terms/",
