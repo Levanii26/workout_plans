d2f23fc65d3697da7fddb6164f46b046092d3139
Levanchutlashvili <131532268+Levanii26@users.noreply.github.com>
2024-03-09 23:00:02 +0400

    Create models for WorkoutPlan, ExercisePlan, and ExerciseInstance

--- plans/models.py
+++ plans/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Exercise(models.Model):
@@ -8,3 +9,24 @@ class Exercise(models.Model):
 
     def __str__(self):
         return self.name
+    
+class WorkoutPlan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    frequency = models.IntegerField()
+    duration = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class ExercisePlan(models.Model):
+    workout_plan = models.ForeignKey(WorkoutPlan, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    repetitions = models.IntegerField()
+    sets = models.IntegerField()
+
+class ExerciseInstance(models.Model):
+    exercise_plan = models.ForeignKey(ExercisePlan, on_delete=models.CASCADE)
+    completed_repetitions = models.IntegerField(default=0)
+    completed_sets = models.IntegerField(default=0)
