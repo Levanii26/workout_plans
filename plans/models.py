812cd1a9e6af88b30438789f420a7d424d1ec6f8
Levanchutlashvili <131532268+Levanii26@users.noreply.github.com>
2024-03-09 23:11:52 +0400

    Create models for UserWeight and UserFitnessGoal

--- plans/models.py
+++ plans/models.py
@@ -30,3 +30,20 @@ class ExerciseInstance(models.Model):
     exercise_plan = models.ForeignKey(ExercisePlan, on_delete=models.CASCADE)
     completed_repetitions = models.IntegerField(default=0)
     completed_sets = models.IntegerField(default=0)
+
+class UserWeight(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    weight = models.FloatField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s weight on {self.date}"
+
+class UserFitnessGoal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    goal = models.CharField(max_length=100)
+    target = models.FloatField()
+    achieved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}'s fitness goal: {self.goal}"
