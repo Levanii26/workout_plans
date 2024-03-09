9912552f718c11061969a2326d5b03c18eae31f8
Levanchutlashvili <131532268+Levanii26@users.noreply.github.com>
2024-03-09 22:57:13 +0400

    define the UserSerializer

--- plans/models.py
+++ plans/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    instructions = models.TextField()
+    target_muscles = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
